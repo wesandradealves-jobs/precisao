e28226468314cc494c742d3549589ac063a8e6f9
Wesley Alves <wesandradealves@gmail.com>
2018-08-05 08:52:57 -0300

    - Ajustes na google api

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,18 +6,6 @@ function _closeMenu(){
     $(".tcon-transform").removeClass("tcon-transform"),
     $(".-on").removeClass("-on")    
 }
-function init_map(){
-    var myOptions = {
-      zoom:17,
-      center: new google.maps.LatLng(-23.5676529,-46.5489975), 
-      disableDefaultUI: true,
-      mapTypeId: google.maps.MapTypeId.TERRAIN
-    };
-    map = new google.maps.Map(document.getElementById('googleMap'), myOptions);
-    marker = new google.maps.Marker({
-        map: map,position: new google.maps.LatLng(-23.5676529,-46.5489975)
-    });
-}
 $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
         loop:false,
@@ -38,7 +26,6 @@ $(document).ready(function () {
     $(window).resize(function(){
         _closeMenu()
     });
-    google.maps.event.addDomListener(window, 'load', init_map);
 });
       
       
