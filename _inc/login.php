ea95f243f9429fa70e1dda37618764c62049dbf1
Wesley Alves <wesandradealves@gmail.com>
2018-08-06 19:06:02 -0300

    - base da dashboard; - redirect se acessar a login e nao tiver deslogado (ainda houver sessao);

--- _inc/login.php
+++ _inc/login.php
@@ -9,8 +9,6 @@
                 $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
                 $responseData = json_decode($verifyResponse);
                 if($responseData->success){
-                    $errMsg = "Valid reCAPTCHA.";
-
                     $login = mysqli_real_escape_string($conn, $_POST['login']); //Escapar de caracteres especiais, como aspas, prevenindo SQL injection
                     $senha = mysqli_real_escape_string($conn, $_POST['senha']);
                     $senha = md5($senha);
--- login.php
+++ login.php
@@ -1,5 +1,8 @@
 <?php
   session_start();
+  if(isset($_SESSION['login'])){
+    header("Location: dashboard.php");
+  }
 ?>
 
 <!DOCTYPE html>
